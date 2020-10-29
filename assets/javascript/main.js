26f149176c3765d5cb9d9fcdaceae7c9f6486dd7
jakubdonovan <jakubdonovan@protonmail.com>
2020-10-29 02:06:38 +0000

    Initial version of the app uploaded. Will work on it further in the future.

---
+++ assets/javascript/main.js
@@ -0,0 +1,141 @@
+// Variables
+let gridSize = 60 // 60x60 by default
+
+
+// Selectors
+const resizeGrid = document.querySelector('.resize-grid')
+const resetGrid = document.querySelector('.reset-grid')
+const grid = document.querySelector('.grid')
+const gridCells = grid.childNodes
+
+
+// Colours
+const rubberButton = document.querySelector('.rubber')
+const purpleButton = document.querySelector('.purple')
+const redButton = document.querySelector('.red')
+const orangeButton = document.querySelector('.orange')
+const greenButton = document.querySelector('.green')
+const blueButton = document.querySelector('.blue')
+const blackButton = document.querySelector('.black')
+
+
+// Listeners
+resizeGrid.addEventListener('click', () => {
+    gridSize = parseInt(prompt('Enter your grid size'))
+    clearGrid(grid)
+    createGrid(gridSize)
+})
+resetGrid.addEventListener('click', () => {
+    clearGrid(grid)
+    createGrid(gridSize)
+})
+rubberButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#C0C0C0')
+})
+purpleButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#80006E')
+})
+redButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#EC021F')
+})
+orangeButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#EC7421')
+})
+greenButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#9BCC56')
+})
+blueButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#5AA3D2')
+})
+blackButton.addEventListener('click', () => {
+    cellHighlighter(gridCells, '#303032')
+})
+
+
+// Functions 
+const createGrid = gridSize => {
+    grid.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`
+    
+    for(let i = 0; (gridSize * gridSize) > i; i++){
+        let cell = document.createElement("div")
+        cell.className = "grid-cell"
+        cell.id = i;
+        grid.appendChild(cell)
+    }
+
+    cellHighlighter(gridCells)
+}
+
+const clearGrid = (parent) => {
+    while (parent.lastChild) parent.removeChild(parent.lastChild)
+}
+
+const cellHighlighter = (gridCells, colour) => {
+    gridCells.forEach(cell => {
+        cell.addEventListener('mouseover', function() {
+            this.style.background = colour
+        })
+    })
+}
+
+
+
+// Initialize
+createGrid(gridSize)
+
+
+
+
+
+
+
+
+
+// /////////////////////
+// // TO FINISH
+// ////////////////////
+
+
+// // Create cursor
+// let createCursor = document.createElement("div")
+// createCursor.className = "cursor"
+// canvas.firstChild.appendChild(createCursor)
+
+// // Style cursor
+// const cursor = document.querySelector(".cursor")
+// cursor.style.height = '100%'
+// cursor.style.width = '100%'
+// cursor.style.background = '#000000'
+
+// // Move cursor
+// let currentCursorPosition = 0
+// let nextCursorPosition = (currentCursorPosition) + gridSize
+
+// window.addEventListener('keydown', event => {
+
+//     const findCursorMoveDown = () => {
+//         canvas.childNodes.forEach(cell => {
+//             return cell.hasChildNodes() ? currentCursorPosition = parseInt(cell.id) : false
+//         })
+
+//         canvas.children[nextCursorPosition].appendChild(cursor)
+//     }
+
+
+//     switch(event.key){
+//         // case 'ArrowUp':
+//         //     break
+
+//         case 'ArrowDown':
+//             findCursorMoveDown()
+
+//             break
+//         // case 'ArrowLeft':
+//         //     break
+//         // case 'ArrowRight':
+//         //     break
+//     }
+// })
+
+
+
